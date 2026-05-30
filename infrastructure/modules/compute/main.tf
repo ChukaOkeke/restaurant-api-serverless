d932b5577d732b2a1af44cc4d2b2e79dc465916e
ChukaOkeke <chukaookeke@gmail.com>
2026-05-30 02:29:16 +0100

    fix(lambda): override checkov on concurrency limits

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -176,7 +176,8 @@ resource "aws_lambda_function" "web_api" {
   # checkov:skip=CKV_AWS_272:Code signing is bypassed for this environment. Pipeline integrity is maintained via GitHub Actions OIDC identity validation and branch protection rules.
   # checkov:skip=CKV_AWS_173:KMS encryption is bypassed in dev to eliminate custom key costs; default cloud security is sufficient
   # checkov:skip=CKV_AWS_116:Uses redrive policy with SQS DLQ for handling failed events instead of Lambda Destinations to allow for easier debugging and reprocessing of failed events during development without needing to set up additional infrastructure components.
-
+  # checkov:skip=CKV_AWS_115:Concurrency limit is intentionally not set due to AWS Account Quota safety mechanism
+  
   depends_on = [aws_cloudwatch_log_group.api_logs]
 }
 
@@ -221,6 +222,7 @@ resource "aws_lambda_function" "queue_worker" {
   # checkov:skip=CKV_AWS_272:Code signing is bypassed for this environment. Pipeline integrity is maintained via GitHub Actions OIDC identity validation and branch protection rules.
   # checkov:skip=CKV_AWS_173:KMS encryption is bypassed in dev to eliminate custom key costs; default cloud security is sufficient
   # checkov:skip=CKV_AWS_116:Uses redrive policy with SQS DLQ for handling failed events instead of Lambda Destinations to allow for easier debugging and reprocessing of failed events during development without needing to set up additional infrastructure components.
+  # checkov:skip=CKV_AWS_115:Concurrency limit is intentionally not set due to AWS Account Quota safety mechanism
   depends_on = [aws_cloudwatch_log_group.worker_logs]
 }
 
