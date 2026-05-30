40c49cea9c875072ec7d0613493875a646e4c080
ChukaOkeke <chukaookeke@gmail.com>
2026-05-30 01:21:30 +0100

    fix: lint code

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -5,7 +5,7 @@
 
 # Primary Regional Provider
 provider "aws" {
-  region  = var.aws_primary_region
+  region = var.aws_primary_region
   # Access keys can be set in the environment variables or through the AWS CLI configuration
 
   # Cascades the granular tagging strategy down to every single sub-resource
@@ -21,8 +21,8 @@ provider "aws" {
 
 # Global Edge Provider (For CloudFront WAF & ACM Certificate compliance)
 provider "aws" {
-  alias   = "us_east_1"
-  region  = var.aws_cloudfront_compliance_region
+  alias  = "us_east_1"
+  region = var.aws_cloudfront_compliance_region
 
   default_tags {
     tags = {
